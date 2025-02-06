cd7799cb5d48bac41f289183b8e6256655fb79c7
pben95 <luapttenneb95@gmail.com>
2025-02-06 00:28:31 -0500

    CLI updates

--- cleaning.py
+++ cleaning.py
@@ -5,14 +5,53 @@ from datetime import datetime, timedelta
 
 txt_files = [f for f in glob.glob(os.path.join("input", "*.txt"))]  #Add data files to "input" folder
 
-os.makedirs("output", exist_ok=True)  #Cleaned files are saved to "output" folder
+daily_names = [
+    "CERT-NY",
+    "CERT-MD",
+    "CERT-IL",
+    "RECERT-NY",
+    "RECERT-MD",
+    "RECERT-IL",
+    "POST-NY",
+    "POST-PA",
+    "POST-MD",
+    "POST-MA",
+    "CORP-NY",
+    "CORP-MD",
+    "CORP-IL"
+]
 
-with open('date.txt', 'r') as file:  #Date is pulled from "date.txt"
-    base_date_str = file.readline().strip()
+base_date_str = datetime.today().strftime('%m/%d/%Y')
 base_date = datetime.strptime(base_date_str, '%m/%d/%Y')
+
+#split parent folder from file path, remove spaces and compare with names in daily_names to try and match daily
+def select_daily():
+    while True:
+        for k, v in enumerate(daily_names):
+            print("(" + str(k) + "): " + v)
+        print("Date: " + str(base_date.strftime('%m/%d/%Y')))
+        print("File to be cleaned: " + str(file))
+        daily_selection = input("Enter the number corresponding to the daily being cleaned, or 'd' to change the date: ").strip()
+        if daily_selection == "d":
+            select_date()
+        try:
+            return daily_names[int(daily_selection)]
+            break
+        except ValueError:
+            print("Invalid daily selected, please try again.")
+
+def select_date():
+    while True:
+        base_date_str = input("Please enter date in MM/DD/YYYY format: ").strip()
+        try:
+            base_date = datetime.strptime(base_date_str, '%m/%d/%Y')
+            break
+        except ValueError:
+            print("Invalid date, please try again.")
 
 for file in txt_files:  #Iterates through all data files from "input"
     input_file = pd.read_csv(file, dtype=str, delimiter=';', header=None)  #Input files are semicolon delimited
+    selected_name = select_daily()
     new_file = pd.DataFrame()
     new_file[0] = input_file[0]
     new_file.insert(1, 'noticedate', base_date.strftime('%m/%d/%Y'))  #Uses date from "date.txt"
@@ -22,9 +61,12 @@ for file in txt_files:  #Iterates through all data files from "input"
     new_file[4] = new_file[4].str.strip()  #Strips spaces
     new_file[5] = input_file[4]
     new_file[6] = input_file[5]
-    new_file[7] = input_file[6]
+    new_file[7] = input_file[6].astype(str)
     new_file[7] = new_file[7].apply(lambda x: x.zfill(5) if len(x) < 5 else x)
     new_file.columns = ['docid', 'noticedate', 'responsedate', 'company', 'address', 'city', 'state', 'zip']  #Headers
-    output_file = os.path.join("output", f"{os.path.splitext(os.path.basename(file))[0]}_cleaned.txt")  #Creates path
-    new_file.to_csv(output_file, sep=';', index=False)  #Saves cleaned file
+    output_name = selected_name + "-" + str(base_date.strftime('%m-%d-%Y')) + "-" + "cleaned.txt"
+    output_dir = "output/" + str(base_date.strftime('%m-%d-%Y'))
+    os.makedirs(output_dir, exist_ok=True)
+    output_file = os.path.join(output_dir, output_name)  #Creates path
+    new_file.to_csv(output_file, sep='\t', index=False)  #Saves cleaned file
     print("Data cleaning completed successfully!")
